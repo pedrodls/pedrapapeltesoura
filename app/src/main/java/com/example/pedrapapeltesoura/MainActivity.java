d645f4c38b43f6aec4055bd62125d8e88dd0db56
PedroSousa2020 <sousapedro747@gmail.com>
2021-05-02 12:28:45 +0000

    First activity done

--- app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.pedrapapeltesoura;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AlertDialogLayout;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void exitGame(View view) {
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("APP");
+        alert.setMessage("Do you want to close APP?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alert.setCancelable(true);
+            }
+        });
+
+        alert.create().show();
     }
 }
