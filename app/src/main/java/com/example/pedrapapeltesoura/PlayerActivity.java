3af93060e49c01ca0c39430861939fba387a2852
PedroSousa2020 <sousapedro747@gmail.com>
2021-05-03 18:02:50 +0100

    App finished

--- app/src/main/java/com/example/pedrapapeltesoura/PlayerActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura/PlayerActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -110,4 +111,10 @@ public class PlayerActivity extends AppCompatActivity {
         setBtnVisible();
     }
 
+    public void openStartScreen(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+    }
 }
